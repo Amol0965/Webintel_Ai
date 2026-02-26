40a6638e639ce986535d6f04fab22ac00f9d8888
Amol0965 <amolchandna9760@gmail.com>
2026-02-26 23:41:53 +0530

    Fix import path for FAISS in main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 import validators
 
