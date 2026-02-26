e5d7d988bf4c24d2409229c701f24eaec53571bb
Amol0965 <amolchandna9760@gmail.com>
2026-02-26 23:38:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
